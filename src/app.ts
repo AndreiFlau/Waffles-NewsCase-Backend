af6ff0267d6ad48b887b73adefb54d51acc68b27
Andrei <andreiflausino@live.com>
2025-02-22 21:16:31 -0300

    allow the frontend on cors

--- src/app.ts
+++ src/app.ts
@@ -19,7 +19,12 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174", "https://waffles-newscase-backend-production.up.railway.app/"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:5174",
+      "https://waffles-newscase-backend-production.up.railway.app/",
+      "https://waffles-news-case-frontend.vercel.app/",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
